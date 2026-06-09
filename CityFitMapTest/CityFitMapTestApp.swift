055aef6c47cd4f70d703351b0c52e4de3717e587
Kenneth <liekennie@gmail.com>
2026-06-09 16:20:34 +0800

    Initial Commit

---
+++ CityFitMapTest/CityFitMapTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityFitMapTestApp.swift
+//  CityFitMapTest
+//
+//  Created by 30 on 2026/6/9.
+//
+
+import SwiftUI
+
+@main
+struct CityFitMapTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
